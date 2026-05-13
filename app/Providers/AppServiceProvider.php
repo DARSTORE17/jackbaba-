35629f4117b037c3f8ac4bd24fa4945dc2fb860f
DARSTORE17 <dt7480319@gmail.com>
2026-05-13 19:10:03 +0300

    Add URL scheme enforcement for production and trust proxies middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Services\SystemSettings;
 use App\Models\Category;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Schema::defaultStringLength(191);
 
         $systemSettings = SystemSettings::load();
